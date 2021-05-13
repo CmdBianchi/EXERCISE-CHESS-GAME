edb2d87473c0c8c415a2f2a55e02459fe68bdbe4
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-13 20:37:25 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE CHESS GAME/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EXERCISE_CHESS_GAME.Tabuleiro;
+namespace EXERCISE_CHESS_GAME {
+    class Program {
+        static void Main(string[] args) {
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posição: "+P);
+
+            Console.ReadLine();
+        }
+    }
+}
